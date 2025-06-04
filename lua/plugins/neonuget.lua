fa132b671c32f6741476b8d30abdae8bc2f664e0
MonsieurTib <tibo.lecoq@gmail.com>
2025-06-04 19:33:04 +0200

    wip

--- lua/plugins/neonuget.lua
+++ lua/plugins/neonuget.lua
@@ -1,7 +1,5 @@
 return {
 	"MonsieurTib/neonuget",
-	commit = "ae5c450e9864a10dc6e903a036f5c34b964af17b",
-	branch = "4-failed-to-parse-package-metadata-when-it-actually-exists",
 	config = function()
 		require("neonuget").setup({})
 	end,
